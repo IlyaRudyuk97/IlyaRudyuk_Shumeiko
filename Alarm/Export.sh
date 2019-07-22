24acf6910790372c973dc8d786292b75fee1c80d
Илья <rudilya944@gmail.com>
2019-07-22 21:50:45 +0700

    Начало backup

--- Alarm/Export.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-croncmd="/home/me/myfunction myargs > /home/me/myfunction.log 2>&1"
-cronjob="0 */15 * * * $croncmd"
-cat <(fgrep -i -v "$croncmd" <(crontab -l)) <(echo "$cronjob") | crontab -
